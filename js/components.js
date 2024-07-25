071e1b2b37ffb249b7ee13b86e9d5c94ebe65298
Ronit <butironit@gmail.com>
2024-07-25 00:15:42 -0700

    created components

---
+++ js/components.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  // Load header
+  $('#header-container').load('components/header.html');
+
+  // Load footer
+  $('#footer-container').load('components/footer.html');
+
+  // Load background
+  $('#background-container').load('components/background.html');
+
+  // Load waitlist form
+  $('#waitlist-form-container').load('components/waitlist-form.html');
+});
